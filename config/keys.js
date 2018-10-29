9068cf9ba58a86c2355f5f36f1783a3164afcff6
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-10-29 10:47:11 -0400

    keys and users

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://test:test@ds229290.mlab.com:29290/todo-mcglynn'
+    mongoURI: 'mongodb://test:test@ds229290.mlab.com:29290/todo-mcglynn',
+    secretOrKey: 'secret'
 };
